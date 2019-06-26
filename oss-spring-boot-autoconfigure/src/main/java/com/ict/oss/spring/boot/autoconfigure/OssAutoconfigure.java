bb4919cfd6645f996ab617155ac664e22ee2b407
CLAYJJ <1198201587@qq.com>
2019-06-26 23:10:09 +0800

    init

--- oss-spring-boot-autoconfigure/src/main/java/com/ict/oss/spring/boot/autoconfigure/OssAutoconfigure.java
+++ oss-spring-boot-autoconfigure/src/main/java/com/ict/oss/spring/boot/autoconfigure/OssAutoconfigure.java
@@ -1,5 +1,18 @@
 package com.ict.oss.spring.boot.autoconfigure;
 
+import com.aliyun.oss.OSSClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(OssProperties.class)
 public class OssAutoconfigure {
 
+    @Bean
+    @ConditionalOnMissingBean
+    public OSSClient ossClient(OssProperties ossProperties) {
+        return new OSSClient(ossProperties.getEndpoint(), ossProperties.getAccessKeyId(), ossProperties.getAccessKeySecret());
+    }
 }
